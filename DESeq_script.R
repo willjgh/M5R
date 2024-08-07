efb026d525a8244ded13c1f1980f1cf2e7e1cb38
willjgh <wjh20@ic.ac.uk>
2024-08-07 20:07:10 +0100

    Differential sequencing script setup.

---
+++ DESeq_script.R
@@ -0,0 +1,28 @@
+library(DESeq2)
+
+# load data
+counts_data <- read.csv('counts_combined.csv', row.names = "X")
+column_data <- read.csv('column_data.csv', row.names = "X")
+
+# DESeqDataSet object
+dds <- DESeqDataSetFromMatrix(countData = counts_data,
+                       colData = column_data,
+                       design = ~ status)
+
+# optional filtering of genes with low counts
+#keep <- rowSums(counts(dds)) >= 1
+#dds <- dds[keep,]
+
+# set factor level e.g. status: original vs perturbed cells
+dds$status <- relevel(dds$status, ref = "original")
+
+# run DESeq
+dds <- DESeq(dds)
+
+# results
+res <- results(dds)
+summary(res)
+
+# plotting
+plotMA(res)
+
