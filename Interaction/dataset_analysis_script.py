515b7aa574153733ce82f91c8c445a01ce534a26
willjgh <williamjgh2000@gmail.com>
2024-11-23 22:06:53 +0000

    Initial setup of analysis script for new data

---
+++ Interaction/dataset_analysis_script.py
@@ -0,0 +1,31 @@
+'''
+Script to analyse simulated datasets, using methods to detect interaction between gene-pairs from a stationary sample
+
+Settings:
+- file name of dataset of counts to be analysed
+- file name of json file containing results
+- threshold for bootstrap (confidence interval truncation)
+- method used to analyse the dataset: min, hyp, corr
+- capture efficiency vector
+- pre-computed truncations (for same capture efficiency)
+- other settings for chosen method (time limit, MIPGap, BestBdThresh, etc)
+
+Output:
+- Stores a dictionary (as json with given name) containing the results: interaction detected, time taken, etc for each gene-pair
+'''
+
+# ------------------------------------------------
+# Settings
+# ------------------------------------------------
+
+# ------------------------------------------------
+# Analysis
+# ------------------------------------------------
+
+# ------------------------------------------------
+# Results
+# ------------------------------------------------
+
+# ------------------------------------------------
+# Functions
+# ------------------------------------------------
